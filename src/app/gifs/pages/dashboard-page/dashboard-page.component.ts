782b8f1bf8097b01f172522468509b5b021ddb6c
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-04 12:34:09 -0600

    Create new component Side menu

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GifsSideMenuHeaderComponent } from '../../components/gifs-side-menu-header/gifs-side-menu-header.component';
-import { GifsSideMenuOptionsComponent } from '../../components/gifs-side-menu-options/gifs-side-menu-options.component';
+import { GifsSideMenuComponent } from '../../components/gifs-side-menu/gifs-side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [
-    RouterOutlet,
-    GifsSideMenuOptionsComponent,
-    GifsSideMenuHeaderComponent,
-  ],
+  imports: [RouterOutlet, GifsSideMenuComponent],
   templateUrl: './dashboard-page.component.html',
 })
 export default class DashboardPageComponent {}
