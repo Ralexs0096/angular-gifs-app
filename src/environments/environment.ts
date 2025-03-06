6cf7efc085d2d079440ced5f694bfafd22eb4ebc
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-06 17:15:39 -0600

    Update our environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   companyName: 'Gifs',
   companySlogan: 'Handle your gifs',
+
+  giphyApiKey: 'EKN0BC0cqFIwvPfquZRJYwD2Gb7dWN91',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
