41274a3426da3f2e77f24910e18f1f5a2e0e0215
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-06 19:26:35 -0600

    Display the search history

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { GifService } from '../../services/gifs.service';
 
 interface MenuOption {
   label: string;
@@ -13,6 +14,8 @@ interface MenuOption {
   templateUrl: './gifs-side-menu-options.component.html',
 })
 export class GifsSideMenuOptionsComponent {
+  gifService = inject(GifService);
+
   menuOptions: MenuOption[] = [
     {
       icon: 'fa-solid fa-chart-line',
