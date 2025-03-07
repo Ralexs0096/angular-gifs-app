69ca86b9271edb9c086675742bc026cf72c30b19
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-06 19:11:30 -0600

    Define a new service method searchGifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -30,5 +31,17 @@ export class GifService {
         this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
       });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          q: query,
+        },
+      })
+      .pipe(map(({ data }) => GifMapper.mapGiphyItemsToGifArray(data)));
   }
 }
