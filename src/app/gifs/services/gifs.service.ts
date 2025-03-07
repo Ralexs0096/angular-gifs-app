21e4a7bc6f59c829c632704989c5493301154ccb
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-06 19:26:23 -0600

    Create the search history

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -12,6 +12,9 @@ export class GifService {
 
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
+
+  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
   constructor() {
     this.loadTrendingGifs();
@@ -42,6 +45,15 @@ export class GifService {
           q: query,
         },
       })
-      .pipe(map(({ data }) => GifMapper.mapGiphyItemsToGifArray(data)));
+      .pipe(
+        map(({ data }) => GifMapper.mapGiphyItemsToGifArray(data)),
+        // History
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase().trim()]: items,
+          }));
+        })
+      );
   }
 }
