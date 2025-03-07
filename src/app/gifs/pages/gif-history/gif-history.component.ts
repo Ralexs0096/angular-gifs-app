8e02110cab2b1bf21f832759de2ce67116e0af75
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-06 19:39:54 -0600

    Get the query key from the url using the ActivatedRoute

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'gif-history',
   imports: [],
   templateUrl: './gif-history.component.html',
 })
-export default class GifHistoryComponent {}
+export default class GifHistoryComponent {
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(map((params) => params['key']))
+  );
+}
