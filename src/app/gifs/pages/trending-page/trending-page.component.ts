7faa8a16312dd10e311ec25ca7750ced11c0b80f
Alex Ruiz <ralexs.acu@gmail.com>
2026-02-02 22:01:36 -0600

    Refactor TrendingPageComponent to implement AfterViewInit and manage scroll state

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,14 +1,29 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
 
   scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(_: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -16,6 +31,9 @@ export default class TrendingPageComponent {
 
     const { scrollTop, clientHeight, scrollHeight } = scrollDiv;
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
     }
