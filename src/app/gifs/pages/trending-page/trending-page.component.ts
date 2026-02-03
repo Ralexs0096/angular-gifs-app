8d2df73d2af7f787fa9c7afeaed2cbfa81b4ef82
Alex Ruiz <ralexs.acu@gmail.com>
2026-02-02 21:48:43 -0600

    Calculate scroll position to call the next page on the scroll event

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,12 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { GifsListComponent } from '../../components/gifs-list/gifs-list.component';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifsListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifService = inject(GifService);
+
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  onScroll(_: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    const { scrollTop, clientHeight, scrollHeight } = scrollDiv;
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    if (isAtBottom) {
+      this.gifService.loadTrendingGifs();
+    }
+  }
 }
